2a33a8db5356b87b81388b416ad02907a0e13e11
shhashi <55823354+shhashi@users.noreply.github.com>
2025-02-06 21:33:25 +0900

    Routes の構成を変更

--- apps/web/src/main/kotlin/shhashi/practice/i20250126/presentation/routes/Routes.kt
+++ apps/web/src/main/kotlin/shhashi/practice/i20250126/presentation/routes/Routes.kt
@@ -1,25 +1,15 @@
 package shhashi.practice.i20250126.presentation.routes
 
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
 import io.ktor.server.http.content.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.thymeleaf.*
+import shhashi.practice.i20250126.presentation.routes.login.LoginRoutes.Companion.loginRoutes
 
 fun Application.configureRouting() {
     routing {
         staticResources("/static", "static")
         staticResources("/assets", "static/assets")
 
-        get("/login") {
-            call.respond(ThymeleafContent("built/login/index", emptyMap()))
-        }
-        authenticate("auth-login") {
-            post("/login") {
-                val principal = call.principal<UserIdPrincipal>()
-                call.respondText("Welcome ${principal?.name}")
-            }
-        }
+        loginRoutes()
     }
 }
---
+++ apps/web/src/main/kotlin/shhashi/practice/i20250126/presentation/routes/login/LoginRoutes.kt
@@ -0,0 +1,23 @@
+package shhashi.practice.i20250126.presentation.routes.login
+
+import io.ktor.server.auth.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.thymeleaf.*
+
+class LoginRoutes {
+    companion object {
+        fun Route.loginRoutes() {
+            get("/login") {
+                call.respond(ThymeleafContent("built/login/index", emptyMap()))
+            }
+
+            authenticate("auth-login") {
+                post("/login") {
+                    val principal = call.principal<UserIdPrincipal>()
+                    call.respondText("Welcome ${principal?.name}")
+                }
+            }
+        }
+    }
+}

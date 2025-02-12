7b11017a9652fb5c335c578e6725c79ed1bdfe7a
shhashi <55823354+shhashi@users.noreply.github.com>
2025-02-12 21:56:36 +0900

    SQL ログつきの transaction ラッパーを実装

---
+++ apps/web/src/main/kotlin/shhashi/practice/i20250126/infrastructure/LoggedTransaction.kt
@@ -0,0 +1,17 @@
+package shhashi.practice.i20250126.infrastructure
+
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun <T> loggedTransaction(
+    db: Database? = null,
+    statement: Transaction.() -> T
+): T {
+    return transaction(db) {
+        addLogger(StdOutSqlLogger) // TODO できれば開発環境のみで本番環境には表示しないような設定を設けたい
+        statement()
+    }
+}

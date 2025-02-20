a338ed295f67448aa599978eb0d24d70c84b80b0
shhashi <55823354+shhashi@users.noreply.github.com>
2025-02-21 01:02:16 +0900

    クラス名を複数形から単数形へ修正

--- apps/web/src/main/kotlin/shhashi/practice/i20250126/infrastructure/repository/StatusRepository.kt
+++ apps/web/src/main/kotlin/shhashi/practice/i20250126/infrastructure/repository/StatusRepository.kt
@@ -3,15 +3,16 @@ package shhashi.practice.i20250126.infrastructure.repository
 import org.koin.core.annotation.Single
 import shhashi.practice.i20250126.infrastructure.dao.tables.Status
 import shhashi.practice.i20250126.infrastructure.loggedTransaction
+import shhashi.practice.i20250126.infrastructure.repository.entity.State
 
 @Single
 class StatusRepository {
-    fun find(): List<shhashi.practice.i20250126.infrastructure.repository.entity.Status> {
+    fun find(): List<State> {
         return loggedTransaction {
             Status
                 .select(Status.statusId, Status.statusName)
                 .map {
-                    shhashi.practice.i20250126.infrastructure.repository.entity.Status(
+                    State(
                         statusId = it[Status.statusId],
                         statusName = it[Status.statusName],
                     )
--- apps/web/src/main/kotlin/shhashi/practice/i20250126/infrastructure/repository/entity/State.kt
+++ apps/web/src/main/kotlin/shhashi/practice/i20250126/infrastructure/repository/entity/State.kt
@@ -2,7 +2,7 @@ package shhashi.practice.i20250126.infrastructure.repository.entity
 
 import java.time.OffsetDateTime
 
-data class Status(
+data class State(
     val statusId: Int? = null,
     val statusName: String? = null,
     val createdAt: OffsetDateTime? = null,
